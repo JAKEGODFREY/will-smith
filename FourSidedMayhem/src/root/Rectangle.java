ce7aa9518df522473e6c138b925d5daeb49544fa
kiranNeal <kiran.n.kulkarni@outlook.com>
2016-11-04 12:54:12 +0000

    AHHHHHHHHHHHHHHH

--- FourSidedMayhem/src/root/Rectangle.java
+++ FourSidedMayhem/src/root/Rectangle.java
@@ -1,20 +1,24 @@
-package root;
+package src.root;
 
 
 
 public class Rectangle extends Parallelogram{
 	
-	
-	
+	public Rectangle(Point point1, Point point2, Point point3, Point point4) {
+		super(point1, point2, point3, point4);
+		// TODO Auto-generated constructor stub
+	}
+
+
 	void Rectangle()
 	{
 		
 	}
 	
 	
-	void isRectangle()
+	static boolean isRectangle()
 	{
-		
+		if(isParallelogram(point1, point2, point3, point4)==false)
 		//If all angles are 90 degrees.
 		
 			//If the opposite sides are the same lengths
