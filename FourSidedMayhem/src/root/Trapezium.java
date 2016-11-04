14ccf0425a5250893042414a0cbd73711661bdef
ThomasAnkcorn <thomasankcorn@gmail.com>
2016-11-04 15:03:47 +0000

    fixed merge

--- FourSidedMayhem/src/root/Trapezium.java
+++ FourSidedMayhem/src/root/Trapezium.java
@@ -7,6 +7,10 @@ public class Trapezium extends Quadrilateral{
 	private Point point4;
 	Quadrilateral quadrilateral = new Trapezium(point1, point2, point3, point4);
 	
+	public Trapezium(){
+		
+	}
+	
 	public Trapezium(Point point1, Point point2, Point point3, Point point4){
 		this.point1 = point1;
 		this.point2 = point2;
@@ -34,10 +38,7 @@ public class Trapezium extends Quadrilateral{
 	}
 
 
-	@Override
-	public double getPerimeter() {
-		// TODO Auto-generated method stub
-		return 0;
+
 
 	@Override
 	int parallels() {
@@ -53,36 +54,19 @@ public class Trapezium extends Quadrilateral{
 
 	@Override
 	double getPerimeter() {
-<<<<<<< HEAD
 		return quadrilateral.getLine1().getLength(point1, point2) + quadrilateral.getLine2().getLength(point2, point3) + quadrilateral.getLine3().getLength(point3, point4) + quadrilateral.getLine4().getLength(point4, point1);
-=======
-		return longBase.getLength() + shortBase.getLength() + slantingSide1.getLength() + slantingSide2.getLength();
 
->>>>>>> d95e46dbaa2e1b3d1c3b6e108d4eef4e6986f480
+
 	}
 
 	@Override
 	double getArea() {
-<<<<<<< HEAD
 		double lengthDifference = quadrilateral.getLine1().getLength(point1, point2) - quadrilateral.getLine4().getLength(point4, point1);
 		double height = Math.sqrt((quadrilateral.getLine2().getLength(point2, point3)*quadrilateral.getLine2().getLength(point2, point3))-(lengthDifference*lengthDifference));
 		return ((quadrilateral.getLine1().getLength(point1, point2)+quadrilateral.getLine4().getLength(point4, point1))/2)*height;
-=======
-
-		return 0.0;
 		
 	}
 
 
-	@Override
-	void parallels() {
-		// TODO Auto-generated method stub
-
-
-		double lengthDifference = longBase.getLength() - shortBase.getLength();
-		double height = Math.sqrt((slantingSide1.getLength()*slantingSide1.getLength())-(lengthDifference*lengthDifference));
-		return ((longBase.getLength()+shortBase.getLength())/2)*height;
-
->>>>>>> d95e46dbaa2e1b3d1c3b6e108d4eef4e6986f480
-	}
+	
 }
