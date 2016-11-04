ccd0b38b57e3c844e6987c0487e81b22d3a3168e
stephenk21 <stephenk21@hotmail.com>
2016-11-04 12:26:33 +0000

    Created the rectangle class - Callum

---
+++ FourSidedMayhem/src/root/Rectangle.java
@@ -0,0 +1,29 @@
+package root;
+
+
+
+public class Rectangle extends Parallelogram{
+	
+	
+	
+	void Rectangle()
+	{
+		
+	}
+	
+	
+	void isRectangle()
+	{
+		
+		//If all angles are 90 degrees.
+		
+			//If the opposite sides are the same lengths
+		
+				//It is a rectangle.
+		
+		
+		
+	}
+	
+	
+}
